40acfaf6675128996b6b6b10dce0a403a159dec0
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-02-23 21:14:22 +0100

    clean the execute folder

--- execute/handle_children.c
+++ execute/handle_children.c
@@ -6,52 +6,27 @@
 /*   By: azerfaou <azerfaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 19:49:36 by azerfaou          #+#    #+#             */
-/*   Updated: 2025/02/21 18:33:48 by azerfaou         ###   ########.fr       */
+/*   Updated: 2025/02/23 19:26:26 by azerfaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+// TODO maybe i should change the input of check_fds to take cmd_manager directly
 void	handle_first_child(t_cmd_manager *cmd_manager, int chd_nbr)
 {
 	int	fd_in;
 	int	fd_out;
-	// int	hd_fd;
 
 	fd_in = cmd_manager->cmds[chd_nbr].fd_in;
 	fd_out = cmd_manager->cmds[chd_nbr].fd_out;
-	// TODO maybe i should change the input of check_fds to take cmd_manager directly
 	check_fds(fd_in, fd_out, cmd_manager->shell);
-	// if (fd_in != STDIN_FILENO)
-	// {
-	// 	dup2(fd_in, STDIN_FILENO);
-	// 	close(fd_in);
-	// }
-	// if (cmd_manager->cmds[chd_nbr].hd_filename != NULL
-	// 	&& cmd_manager->cmds[chd_nbr].hd_filename[0] != '\0')
-	// {
-	// 	hd_fd = open(cmd_manager->cmds[chd_nbr].hd_filename, O_RDONLY);
-	// 	// unlink(cmd_manager->cmds[chd_nbr].hd_filename);
-	// 	if (hd_fd < 0)
-	// 	{
-	// 		perror("open hd_filename");
-	// 		exit(EXIT_FAILURE);
-	// 	}
-	// 	dup2(hd_fd, STDIN_FILENO);
-	// 	close(hd_fd);
-	// }
 	setup_input(fd_in, cmd_manager->cmds[chd_nbr].hd_filename, -1);
 	if (cmd_manager->nbr_cmds > 1)
 	{
 		setup_output(fd_out, cmd_manager->pipes[chd_nbr][1]);
 		close_unused_pipes(cmd_manager->pipes, cmd_manager->nbr_cmds, chd_nbr);
 	}
-	// if (cmd_manager->nbr_cmds > 1)
-	// {
-	// 	dup2(cmd_manager->pipes[chd_nbr][1], STDOUT_FILENO);
-	// 	close(cmd_manager->pipes[chd_nbr][1]);
-	// 	close_unused_pipes(cmd_manager->pipes, cmd_manager->nbr_cmds, chd_nbr);
-	// }
 	if (fd_out != STDOUT_FILENO)
 	{
 		dup2(fd_out, STDOUT_FILENO);
@@ -62,86 +37,26 @@ void	handle_first_child(t_cmd_manager *cmd_manager, int chd_nbr)
 void	handle_last_child(t_cmd_manager *cmd_manager, int chd_nbr)
 {
 	int	fd_out;
-	// int	hd_fd;
 
 	fd_out = cmd_manager->cmds[chd_nbr].fd_out;
 	close_unused_pipes(cmd_manager->pipes, cmd_manager->nbr_cmds, chd_nbr);
-	//input
 	setup_input(cmd_manager->cmds[chd_nbr].fd_in,
-		cmd_manager->cmds[chd_nbr].hd_filename,
-		cmd_manager->pipes[chd_nbr - 1][0]);
-	// if (cmd_manager->cmds[chd_nbr].hd_filename != NULL
-	// 	&& cmd_manager->cmds[chd_nbr].hd_filename[0] != '\0')
-	// {
-	// 	hd_fd = open(cmd_manager->cmds[chd_nbr].hd_filename, O_RDONLY);
-	// 	// unlink(cmd_manager->cmds[chd_nbr].hd_filename);
-	// 	if (hd_fd < 0)
-	// 	{
-	// 		perror("open hd_filename");
-	// 		exit(EXIT_FAILURE);
-	// 	}
-	// 	dup2(hd_fd, STDIN_FILENO);
-	// 	close(hd_fd);
-	// }
-	// else if (cmd_manager->cmds[chd_nbr].fd_in > 0)
-	// {
-	// 	dup2(cmd_manager->cmds[chd_nbr].fd_in, STDIN_FILENO);
-	// 	close(cmd_manager->cmds[chd_nbr].fd_in);
-	// }
-	// else
-	// {
-	// 	dup2(cmd_manager->pipes[chd_nbr - 1][0], STDIN_FILENO);
-	// 	close(cmd_manager->pipes[chd_nbr - 1][0]);
-	// }
-	//output
+		cmd_manager->cmds[chd_nbr].hd_filename, cmd_manager->pipes[chd_nbr
+		- 1][0]);
 	setup_output(fd_out, -1);
-	// if (fd_out != STDOUT_FILENO)
-	// {
-	// 	dup2(fd_out, STDOUT_FILENO);
-	// 	close(fd_out);
-	// }
 	cmd_manager->pid = getpid();
 }
 
+// TODO fix this shit NOW
 void	handle_mid_children(t_cmd_manager *cmd_manager, int chd_nbr)
-		// TODO fix this shit NOW
 {
-	int fd_out = cmd_manager->cmds[chd_nbr].fd_out;
-	int	fd_in = cmd_manager->cmds[chd_nbr].fd_in;
-	// int	hd_fd;
+	int	fd_out;
+	int	fd_in;
 
+	fd_in = cmd_manager->cmds[chd_nbr].fd_in;
+	fd_out = cmd_manager->cmds[chd_nbr].fd_out;
 	close_unused_pipes(cmd_manager->pipes, cmd_manager->nbr_cmds, chd_nbr);
-
-	// Handle input
-	setup_input(fd_in, cmd_manager->cmds[chd_nbr].hd_filename, cmd_manager->pipes[chd_nbr - 1][0]);
-	// if (cmd_manager->cmds[chd_nbr].hd_filename != NULL
-	// 	&& cmd_manager->cmds[chd_nbr].hd_filename[0] != '\0')
-	// {
-	// 	hd_fd = open(cmd_manager->cmds[chd_nbr].hd_filename, O_RDONLY);
-	// 	dup2(hd_fd, STDIN_FILENO);
-	// 	close(hd_fd);
-	// }
-	// else if (fd_in > 0)
-	// {
-	// 	dup2(fd_in, STDIN_FILENO);
-	// 	close(fd_in);
-	// }
-	// else
-	// {
-	// 	dup2(cmd_manager->pipes[chd_nbr - 1][0], STDIN_FILENO);
-	// 	close(cmd_manager->pipes[chd_nbr - 1][0]);
-	// }
-
-	// Handle output
+	setup_input(fd_in, cmd_manager->cmds[chd_nbr].hd_filename,
+		cmd_manager->pipes[chd_nbr - 1][0]);
 	setup_output(fd_out, cmd_manager->pipes[chd_nbr][1]);
-	// if (fd_out != STDOUT_FILENO)
-	// {
-	// 	dup2(fd_out, STDOUT_FILENO);
-	// 	close(fd_out);
-	// }
-	// else
-	// {
-	// 	dup2(cmd_manager->pipes[chd_nbr][1], STDOUT_FILENO);
-	// 	close(cmd_manager->pipes[chd_nbr][1]);
-	// }
 }
--- execute/pipex.c
+++ execute/pipex.c
@@ -6,7 +6,7 @@
 /*   By: azerfaou <azerfaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 14:04:47 by azerfaou          #+#    #+#             */
-/*   Updated: 2025/02/21 22:03:39 by azerfaou         ###   ########.fr       */
+/*   Updated: 2025/02/22 19:00:57 by azerfaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,9 +61,9 @@ int	handle_single_command(t_command *cmd, t_shell **shell)
 {
 	if (cmd->path == NULL
 		&& cmd->fd_in > 2)
-	{
 		return (1);
-	}
+	else if (cmd->hd_filename != NULL)
+		return (1);
 	else if (cmd->path == NULL)
 	{
 		ft_putstr_fd(STDERR_FILENO, "bash:");
@@ -87,92 +87,92 @@ int	handle_single_command(t_command *cmd, t_shell **shell)
  * the last child will read from the pipe and write to the output file
  * the middle children will read from the pipe and write to the next one
  */
-void	create_cmd_processes(t_cmd_manager *cmd_manager)
-{
-	int	chd_nbr;
-	int	status;
+// void	create_cmd_processes(t_cmd_manager *cmd_manager)
+// {
+// 	int	chd_nbr;
+// 	int	status;
 
-	chd_nbr = 0;
-	while (chd_nbr < cmd_manager->nbr_cmds)
-	{
-		if (cmd_manager->nbr_cmds == 1)
-		{
-			if (handle_single_command
-				(&cmd_manager->cmds[chd_nbr], cmd_manager->shell))
-				break ;
-			if (cmd_manager->cmds[chd_nbr].path
-				&& is_builtin(cmd_manager->cmds[chd_nbr].path))
-			{
-				execute_builtins(cmd_manager->cmds[chd_nbr].path,
-					cmd_manager->cmds[chd_nbr].args,
-					cmd_manager->shell);
-				chd_nbr++;
-				continue ;
-			}
-		}
-		cmd_manager->pid = fork();
-		if (cmd_manager->pid == -1)
-		{
-			ft_putstr_fd(STDERR_FILENO, "bash: fork error\n");
-			(*(cmd_manager->shell))->exit_status = FORK_ERROR;
-			exit(FORK_ERROR);
-		}
-		if (cmd_manager->pid == 0) // 0 is the child
-		{
-			default_signals();
-			if (chd_nbr == 0)
-				handle_first_child(cmd_manager, chd_nbr);
-			else if (chd_nbr == cmd_manager->nbr_cmds - 1)
-				handle_last_child(cmd_manager, chd_nbr);
-			else
-				handle_mid_children(cmd_manager, chd_nbr);
-			if (cmd_manager->cmds[chd_nbr].path == NULL
-				&& cmd_manager->cmds[chd_nbr].fd_in > 2)
-			{
-				exit(EXIT_SUCCESS);
-			}
-			if (cmd_manager->cmds[chd_nbr].path == NULL)
-			{
-				ft_putstr_fd(STDERR_FILENO, "bash:zzzz");
-				ft_putstr_fd(STDERR_FILENO, ": command not found\n");
-				(*(cmd_manager->shell))->exit_status = COMMAND_NOT_FOUND;
-				exit(COMMAND_NOT_FOUND);
-			}
-			else if (cmd_manager->cmds[chd_nbr].fd_in == -1)
-			{
-				ft_putstr_fd(STDERR_FILENO, "bash: :No such file or directory\n");
-				(*(cmd_manager->shell))->exit_status = OPEN_ERROR;
-				exit(OPEN_ERROR);
-			}
-			else if (is_builtin(cmd_manager->cmds[chd_nbr].path))
-			{
-				execute_builtins(cmd_manager->cmds[chd_nbr].path,
-					cmd_manager->cmds[chd_nbr].args,
-					cmd_manager->shell);
-				exit(EXIT_SUCCESS);
-			}
-			else if (execve(cmd_manager->cmds[chd_nbr].path,
-					cmd_manager->cmds[chd_nbr].args, (*(cmd_manager->shell))->env) == -1)
-			{
-				(*(cmd_manager->shell))->exit_status = COMMAND_NOT_FOUND;
-			}
-			else//TODO should i keep this??
-			{
-				(*(cmd_manager->shell))->exit_status = 0;
-			}
-		}
-		else if (cmd_manager->nbr_cmds == 1)
-		{
-			waitpid(cmd_manager->pid, &status, 0);
-			if (WIFEXITED(status))
-				(*(cmd_manager->shell))->exit_status = status >> 8 & 0xFF;
-			setup_signals();
-		}
-		else
-		{
-			setup_signals();
-		}
-		chd_nbr++;
-	}
-	close_unused_pipes(cmd_manager->pipes, cmd_manager->nbr_cmds, chd_nbr);
-}
+// 	chd_nbr = 0;
+// 	while (chd_nbr < cmd_manager->nbr_cmds)
+// 	{
+// 		if (cmd_manager->nbr_cmds == 1)
+// 		{
+// 			if (handle_single_command
+// 				(&cmd_manager->cmds[chd_nbr], cmd_manager->shell))
+// 				break ;
+// 			if (cmd_manager->cmds[chd_nbr].path
+// 				&& is_builtin(cmd_manager->cmds[chd_nbr].path))
+// 			{
+// 				execute_builtins(cmd_manager->cmds[chd_nbr].path,
+// 					cmd_manager->cmds[chd_nbr].args,
+// 					cmd_manager->shell);
+// 				chd_nbr++;
+// 				continue ;
+// 			}
+// 		}
+// 		cmd_manager->pid = fork();
+// 		if (cmd_manager->pid == -1)
+// 		{
+// 			ft_putstr_fd(STDERR_FILENO, "bash: fork error\n");
+// 			(*(cmd_manager->shell))->exit_status = FORK_ERROR;
+// 			exit(FORK_ERROR);
+// 		}
+// 		if (cmd_manager->pid == 0) // 0 is the child
+// 		{
+// 			default_signals();
+// 			if (chd_nbr == 0)
+// 				handle_first_child(cmd_manager, chd_nbr);
+// 			else if (chd_nbr == cmd_manager->nbr_cmds - 1)
+// 				handle_last_child(cmd_manager, chd_nbr);
+// 			else
+// 				handle_mid_children(cmd_manager, chd_nbr);
+// 			if (cmd_manager->cmds[chd_nbr].path == NULL
+// 				&& cmd_manager->cmds[chd_nbr].fd_in > 2)
+// 			{
+// 				exit(EXIT_SUCCESS);
+// 			}
+// 			if (cmd_manager->cmds[chd_nbr].path == NULL)
+// 			{
+// 				ft_putstr_fd(STDERR_FILENO, "bash:zzzz");
+// 				ft_putstr_fd(STDERR_FILENO, ": command not found\n");
+// 				(*(cmd_manager->shell))->exit_status = COMMAND_NOT_FOUND;
+// 				exit(COMMAND_NOT_FOUND);
+// 			}
+// 			else if (cmd_manager->cmds[chd_nbr].fd_in == -1)
+// 			{
+// 				ft_putstr_fd(STDERR_FILENO, "bash: :No such file or directory\n");
+// 				(*(cmd_manager->shell))->exit_status = OPEN_ERROR;
+// 				exit(OPEN_ERROR);
+// 			}
+// 			else if (is_builtin(cmd_manager->cmds[chd_nbr].path))
+// 			{
+// 				execute_builtins(cmd_manager->cmds[chd_nbr].path,
+// 					cmd_manager->cmds[chd_nbr].args,
+// 					cmd_manager->shell);
+// 				exit(EXIT_SUCCESS);
+// 			}
+// 			else if (execve(cmd_manager->cmds[chd_nbr].path,
+// 					cmd_manager->cmds[chd_nbr].args, (*(cmd_manager->shell))->env) == -1)
+// 			{
+// 				(*(cmd_manager->shell))->exit_status = COMMAND_NOT_FOUND;
+// 			}
+// 			else//TODO should i keep this??
+// 			{
+// 				(*(cmd_manager->shell))->exit_status = 0;
+// 			}
+// 		}
+// 		else if (cmd_manager->nbr_cmds == 1)
+// 		{
+// 			waitpid(cmd_manager->pid, &status, 0);
+// 			if (WIFEXITED(status))
+// 				(*(cmd_manager->shell))->exit_status = status >> 8 & 0xFF;
+// 			setup_signals();
+// 		}
+// 		else
+// 		{
+// 			setup_signals();
+// 		}
+// 		chd_nbr++;
+// 	}
+// 	close_unused_pipes(cmd_manager->pipes, cmd_manager->nbr_cmds, chd_nbr);
+// }
